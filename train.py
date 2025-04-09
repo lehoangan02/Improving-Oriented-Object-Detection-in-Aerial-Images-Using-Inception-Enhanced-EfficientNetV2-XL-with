7a992aee546ab6faddb5b8f6b27100cc0a24bbf8
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-04-09 16:46:40 +0700

    loss aux ratio 0.1

--- train.py
+++ train.py
@@ -113,7 +113,7 @@ class TrainModule(object):
                 self.model = nn.DataParallel(self.model)
         self.model.to(self.device)
 
-        criterion = loss.LossAll_aux(0.2)
+        criterion = loss.LossAll_aux(0.1)
         print('Setting up data...')
 
         dataset_module = self.dataset[args.dataset]
