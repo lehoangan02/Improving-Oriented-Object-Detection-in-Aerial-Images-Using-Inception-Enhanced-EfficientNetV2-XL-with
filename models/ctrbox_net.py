1325135a71319279e8a80e5d3428e234f58b4b87
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-04-19 21:08:56 +0700

    explicit corners

--- models/ctrbox_net.py
+++ models/ctrbox_net.py
@@ -555,7 +555,7 @@ class CTRBOX_ImplicitCornersV2(nn.Module):
             if 'hm' in head or 'cls' in head:
                 dec_dict[head] = torch.sigmoid(dec_dict[head])
         return dec_dict
-class CTRBOX_ExplicitCorners(nn.Module):
+class CTRBOX_ExplicitCornersV2(nn.Module):
     def __init__(self, heads, pretrained, down_ratio, final_kernel, head_conv):
         super().__init__()
         channels = [3, 64, 256, 512, 1024, 2048]
@@ -1415,3 +1415,130 @@ class CTRBOX_Github_aux(nn.Module):
             if 'hm' in head or 'cls' in head:
                 aux_dict[head] = torch.sigmoid(aux_dict[head])
         return dec_dict, aux_dict
+class CTRBOX_ExplicitCorners(nn.Module):
+    def __init__(self, heads, pretrained, down_ratio, final_kernel, head_conv):
+        super().__init__()
+        channels = [3, 64, 256, 512, 1024, 2048]
+        assert down_ratio in [2, 4, 8, 16]
+        self.l1 = int(np.log2(down_ratio))
+        self.base_network = resnet.resnet152(pretrained=pretrained)
+
+        self.dec_c2 = CombinationModule(512, 256, batch_norm=True)
+        self.dec_c3 = CombinationModule(1024, 512, batch_norm=True)
+        self.dec_c4 = CombinationModule(2048, 1024, batch_norm=True)
+        self.heads = heads
+
+        for head in self.heads:
+            classes = self.heads[head]
+            if head == 'wh':
+                fc = nn.Sequential(mini_inception.MiniInception(),
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(channels[self.l1], head_conv, kernel_size=7, padding=3, bias=True),
+                                   nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(head_conv, 18, kernel_size=7, padding=3, bias=True))
+            else:
+                fc = nn.Sequential(nn.Conv2d(channels[self.l1], head_conv, kernel_size=3, padding=1, bias=True),
+                                   nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(head_conv, classes, kernel_size=final_kernel, stride=1, padding=final_kernel // 2, bias=True))
+            if 'hm' in head:
+                fc[-1].bias.data.fill_(-2.19)
+            else:
+                self.fill_fc_weights(fc)
+
+            self.__setattr__(head, fc)
+
+    def fill_fc_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.base_network(x)
+        c4_combine = self.dec_c4(x[-1], x[-2])
+        c3_combine = self.dec_c3(c4_combine, x[-3])
+        c2_combine = self.dec_c2(c3_combine, x[-4])
+
+        dec_dict = {}
+        for head in self.heads:
+            if head == 'wh':
+                wh_and_corners = self.__getattr__(head)(c2_combine)
+                wh = wh_and_corners[:, :10]
+                corners = wh_and_corners[:, 10:18]
+                processed_tt = wh[:, 0:2] / 2 + corners[:, 2:4] / 4 + corners[:, 4:6] / 4
+                processed_rr = wh[:, 2:4] / 2 + corners[:, 4:6] / 4 + corners[:, 6:8] / 4
+                processed_bb = wh[:, 4:6] / 2 + corners[:, 0:2] / 4 + corners[:, 6:8] / 4
+                processed_ll = wh[:, 6:8] / 2 + corners[:, 0:2] / 4 + corners[:, 2:4] / 4
+                width_height = wh[:, 8:10]
+                processed_wh = torch.cat((processed_tt, processed_rr, processed_bb, processed_ll, width_height), dim=1)
+                dec_dict[head] = processed_wh
+            else:
+                dec_dict[head] = self.__getattr__(head)(c2_combine)
+            if 'hm' in head or 'cls' in head:
+                dec_dict[head] = torch.sigmoid(dec_dict[head])
+        return dec_dict
+class CTRBOX_ExplicitCornersV2(nn.Module):
+    def __init__(self, heads, pretrained, down_ratio, final_kernel, head_conv):
+        super().__init__()
+        channels = [3, 64, 256, 512, 1024, 2048]
+        assert down_ratio in [2, 4, 8, 16]
+        self.l1 = int(np.log2(down_ratio))
+        self.base_network = resnet.resnet152(pretrained=pretrained)
+
+        self.dec_c2 = CombinationModule(512, 256, batch_norm=True)
+        self.dec_c3 = CombinationModule(1024, 512, batch_norm=True)
+        self.dec_c4 = CombinationModule(2048, 1024, batch_norm=True)
+        self.heads = heads
+
+        for head in self.heads:
+            classes = self.heads[head]
+            if head == 'wh':
+                fc = nn.Sequential(mini_inception.MiniInception(),
+                                   nn.ReLU(inplace=True),
+                                   mini_inception.MiniInception(),
+                                   nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(head_conv, 18, kernel_size=1, padding=0, stride=1, bias=True))
+            else:
+                fc = nn.Sequential(nn.Conv2d(channels[self.l1], head_conv, kernel_size=3, padding=1, bias=True),
+                                   nn.BatchNorm2d(head_conv),   # BN not used in the paper, but would help stable training
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(head_conv, classes, kernel_size=final_kernel, stride=1, padding=final_kernel // 2, bias=True))
+            if 'hm' in head:
+                fc[-1].bias.data.fill_(-2.19)
+            else:
+                self.fill_fc_weights(fc)
+
+            self.__setattr__(head, fc)
+
+
+    def fill_fc_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.base_network(x)
+        c4_combine = self.dec_c4(x[-1], x[-2])
+        c3_combine = self.dec_c3(c4_combine, x[-3])
+        c2_combine = self.dec_c2(c3_combine, x[-4])
+
+        dec_dict = {}
+        for head in self.heads:
+            if head == 'wh':
+                wh_and_corners = self.__getattr__(head)(c2_combine)
+                wh = wh_and_corners[:, :10]
+                corners = wh_and_corners[:, 10:18]
+                processed_tt = wh[:, 0:2] / 2 + corners[:, 2:4] / 4 + corners[:, 4:6] / 4
+                processed_rr = wh[:, 2:4] / 2 + corners[:, 4:6] / 4 + corners[:, 6:8] / 4
+                processed_bb = wh[:, 4:6] / 2 + corners[:, 0:2] / 4 + corners[:, 6:8] / 4
+                processed_ll = wh[:, 6:8] / 2 + corners[:, 0:2] / 4 + corners[:, 2:4] / 4
+                width_height = wh[:, 8:10]
+                processed_wh = torch.cat((processed_tt, processed_rr, processed_bb, processed_ll, width_height), dim=1)
+                dec_dict[head] = processed_wh
+            else:
+                dec_dict[head] = self.__getattr__(head)(c2_combine)
+            if 'hm' in head or 'cls' in head:
+                dec_dict[head] = torch.sigmoid(dec_dict[head])
+        return dec_dict
