64189a18a9d33542d652c0d1ebe1bb8c2da87a46
hvtham <hvtham@mgmt01>
2025-04-28 23:05:46 +0000

    eval job

--- Custom/Scripts/evaluate_models.sh
+++ Custom/Scripts/evaluate_models.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --output=output2.log
+#SBATCH --gres=gpu:1
+#SBATCH --mem=60G
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=4
 
-# Things you need to change:
-# 1. data_dir: Path to the directory containing the validation data
-# 2. model_dir: Path to the directory containing the model weights
+source /home/hvtham/miniconda3/bin/activate
+conda activate myenv
+cd /media02/hvtham/group3/BBAVectors-Oriented-Object-Detection
+git pull
+git switch conventional
+cd Custom/Scripts
 
-# How to run:
-# Make sure you are using Git bash if you are on Windows
-# cd Custom/Scripts
-# chmod +x evaluate_models.sh
-# ./evaluate_models.sh
 echo "Running evaluation for all models in the model directory..."
 # Define variables
-data_dir="./datasets/Validate_DOTA_1_0.5"
-# data_dir="./datasets/MiniTrainV1.1" # testing
+validate_data_dir="./datasets/Validate_DOTA_1_0.5"
+training_data_dir="./datasets/trainsplit"
 # Directory structure:
 # -|datasets
 # ---|your_validation_data
@@ -22,7 +26,7 @@ conf_thresh=0.1
 batch_size=16
 dataset="dota"
 phase="eval"
-model_dir="efficientnetv3"
+model_dir="combinedV1_chpc"
 # Directory structure:
 # -|weights_dota
 # ---| your_model_weights
@@ -31,11 +35,11 @@ model_dir="efficientnetv3"
 # -----| ...
 eval_script="dota_evaluation_task1.py"
 eval_dir="datasets/DOTA_devkit"
-result_dir="Result/efficientnetv3"
+result_dir="Result/combinedV1_chpc"
 # A folder named "Result" will be created in the current directory to store the evaluation results
 
 # Array of model epochs to evaluate
-epochs=(6)
+epochs=(1 2 3)
 cd ../..
 
 # Create the result directory if it doesn't exist
@@ -45,10 +49,18 @@ mkdir -p "$result_dir"
 # Loop through the epochs and run the evaluation
 for epoch in "${epochs[@]}"; do
     model_path="${model_dir}/model_${epoch}.pth"
-    echo "Running evaluation for model at epoch ${epoch}..."
-    python main.py --data_dir "$data_dir" --conf_thresh "$conf_thresh" --batch_size "$batch_size" --dataset "$dataset" --phase "$phase" --resume "$model_path"
+    echo "Running validate evaluation for model at epoch ${epoch}..."
+    python3 main.py --data_dir "$validate_data_dir" --conf_thresh "$conf_thresh" --batch_size "$batch_size" --dataset "$dataset" --phase "$phase" --resume "$model_path"
     
     # Change directory to evaluation script location and run evaluation
     echo "Running DOTA evaluation for model at epoch ${epoch}..."
-    (cd "$eval_dir" && python "$eval_script") | tee "$result_dir/evaluation_result_for_epoch_${epoch}.txt"
+    (cd "$eval_dir" && python "$eval_script") | tee "$result_dir/validate_evaluation_result_for_epoch_${epoch}.txt"
+
+    echo "Running training evaluation for model at epoch ${epoch}..."
+    python3 main.py --data_dir "$training_data_dir" --conf_thresh "$conf_thresh" --batch_size "$batch_size" --dataset "$dataset" --phase "$phase" --resume "$model_path"
+
+    # Change directory to evaluation script location and run evaluation
+    echo "Running DOTA evaluation for model at epoch ${epoch}..."
+    (cd "$eval_dir" && python "$eval_script") | tee "$result_dir/training_evaluation_result_for_epoch_${epoch}.txt"
+
 done
